81d93aeb8c7416927e23dfd7ccb4276110013ad1
mmd <mmd@DESKTOP-NODIGUH>
2020-12-15 00:33:01 +0330

    initial context and model

---
+++ MmdSolution1/CleanArch.Data/Context/UniversityDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CleanArch.Domain.Models;
+
+namespace CleanArch.Data.Context
+{
+    public class UniversityDbContext : DbContext
+    {
+        public UniversityDbContext(DbContextOptions<UniversityDbContext> options)
+        : base(options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
---
+++ MmdSolution1/CleanArch.Infra.Data/Context/UniversityDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Context
+{
+    public class UniversityDBContext : DbContext
+    {
+    }
+}
